dde5d3e432a57d6c5be6ef522b2e0570d8556676
sandsrini <87754862+sandsrini@users.noreply.github.com>
2021-10-12 13:24:02 +0530

    Add files via upload

---
+++ Day6 Delegates/Assignment/Day6 Handson/Day6 Handson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day6_Handson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
